4bdb3ac923405ad145416403be3fe91cd8baa189
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-09-22 12:15:33 +0200

    Revert back to display the game

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import './style.css'
-import {startSpiders} from "./spider";
-// import {setupGame} from "./game";
+import {setupGame} from "./game";
 
 const appRoot = document.querySelector<HTMLDivElement>('#app')!
-// const header = document.createElement("h1")
-// header.innerText = `usplusminus`
-// appRoot.appendChild(header)
-// const gameContainer = document.createElement("div")
-// appRoot.appendChild(gameContainer)
-// setupGame(gameContainer)
 
+const header = document.createElement("h1")
+header.innerText = `usplusminus`
+appRoot.appendChild(header)
 
-const canvas = document.createElement("canvas")
-appRoot.appendChild(canvas)
-startSpiders(canvas)
+const gameContainer = document.createElement("div")
+appRoot.appendChild(gameContainer)
+setupGame(gameContainer)
+
+// import {startSpiders} from "./spider";
+// const canvas = document.createElement("canvas")
+// appRoot.appendChild(canvas)
+// startSpiders(canvas)

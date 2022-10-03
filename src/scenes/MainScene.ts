c7b743d30b34fa6e24ed9b250838e5304f1816b1
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-10-03 22:21:08 +0200

    Remove platforms from the main scene

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -13,10 +13,6 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image(assets.platforms.platform1.key, assets.platforms.platform1.filepath);
-        this.load.image(assets.platforms.platform2.key, assets.platforms.platform2.filepath);
-        this.load.image(assets.platforms.platform3.key, assets.platforms.platform3.filepath);
-        this.load.image(assets.platforms.platform4.key, assets.platforms.platform4.filepath);
         this.load.spritesheet(assets.dude.key, assets.dude.filepath,
             {frameWidth: 32, frameHeight: 48}
         );
@@ -27,19 +23,10 @@ export default class MainScene extends Phaser.Scene {
         this.cameras.main.setZoom(0.5);
         this.cameras.main.centerOn(0, 0);
 
-        const platforms = this.physics.add.staticGroup()
-        platforms.create(400, 568, assets.platforms.platform1.key).setScale(2).refreshBody();
-        platforms.create(600, 400, assets.platforms.platform2.key).setScale(2).refreshBody();
-        platforms.create(50, 250, assets.platforms.platform3.key).setScale(2).refreshBody();
-        platforms.create(750, 220, assets.platforms.platform4.key).setScale(2).refreshBody();
-
-
         this.player = new Player(this)
         this.spider = new Spider(this)
 
         this.cameras.main.startFollow(this.player.sprite, true, 0.09, 0.09);
-
-        this.physics.add.collider(this.player.sprite, platforms)
 
         const points = [
             20, 550,
@@ -54,9 +41,12 @@ export default class MainScene extends Phaser.Scene {
         // path.draw(graphics, 128);
 
         const storyText = "What if you could trace the lineage of ideas from person to person throughout history"
-        path.getSpacedPoints(storyText.length).map((point, idx) =>
-            this.add.text(point.x, point.y, storyText.at(idx) ?? "", {fontSize: '32px', color: semanticColors.primary})
-        )
+        path.getSpacedPoints(storyText.length)
+            .map((point, idx) =>
+                this.add.text(point.x, point.y, storyText.at(idx) ?? "",
+                    {fontSize: '32px', color: semanticColors.primary}
+                )
+            )
     }
 
     update(_time: number, _delta: number) {

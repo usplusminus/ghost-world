4641b0f75dce694b00df703ba3f99a79a07ab5bd
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-10-03 23:10:49 +0200

    Display the dots that the spider traverses through

--- src/gameobjects/Spider.ts
+++ src/gameobjects/Spider.ts
@@ -4,14 +4,18 @@ import {HexColor} from "../scenes/colors";
 type Point = {
     x: number,
     y: number,
-    len?: number,
-    r?: number,
-    t?: number
+}
+
+type Circle = {
+    x: number,
+    y: number,
+    len: number,
+    radius: number,
 }
 
 export class Spider extends Phaser.GameObjects.Graphics {
+    private circles: Circle[];
     private points: Point[];
-    private points2: Point[];
     private seed: number;
     private tx: number;
     private ty: number;
@@ -19,61 +23,68 @@ export class Spider extends Phaser.GameObjects.Graphics {
     private ky: number;
     private walkRadius: Point;
     private r: number;
-    private pointGraphics: Phaser.GameObjects.Graphics;
-    private lineGraphics: Phaser.GameObjects.Graphics;
+    private graphics: Phaser.GameObjects.Graphics;
 
     constructor(scene: Phaser.Scene) {
         super(scene);
 
-        this.pointGraphics = this.scene.add.graphics({lineStyle: {width: 3.0, color: HexColor.white, alpha: 1.0}})
-        this.lineGraphics = this.scene.add.graphics({lineStyle: {width: 0.5, color: HexColor.white, alpha: 1.0}})
+        this.graphics = this.scene.add.graphics({fillStyle: { color: HexColor.white }, lineStyle: {width: 0.5, color: HexColor.white, alpha: 1.0}})
 
-        this.points = many(100, () => {
+
+        // TODO: the points don't go out of (0, 0) -> decenter the playing area in some way or generate then points over a larger distance
+        // const debugGraphics = this.scene.add.graphics({fillStyle: { color: HexColor.white }, lineStyle: {width: 0.5, color: HexColor.white, alpha: 1.0}})
+        // const circle = {
+        //     x: 0,
+        //     y: 0,
+        //     len: 0,
+        //     radius: 10
+        // }
+        // debugGraphics.fillCircle(circle.x, circle.y, circle.radius)
+
+        this.circles = many(1000, () => {
             return {
-                x: randomInRange(innerWidth),
-                y: randomInRange(innerHeight),
+                x: randomInRange(-innerWidth, innerWidth),
+                y: randomInRange(-innerHeight, innerHeight),
                 len: 0,
-                r: 0
+                radius: 0
             };
         });
+        this.circles.forEach(circle => this.drawCircle(circle))
 
-        this.points2 = many(9, (idx) => {
-            return {
-                x: Math.cos((idx / 9) * Math.PI * 2),
-                y: Math.sin((idx / 9) * Math.PI * 2)
-            };
-        });
+        this.points = many(9, (idx) => point(
+            Math.cos((idx / 9) * Math.PI * 2),
+            Math.sin((idx / 9) * Math.PI * 2)
+        ))
 
-
-        this.seed = randomInRange(100)
-        this.tx = randomInRange(innerWidth / 4);
-        this.ty = randomInRange(innerHeight / 4);
+        const maxRadius = 150
+        this.seed = random(100)
+        this.tx = random(innerWidth);
+        this.ty = random(innerHeight);
         this.x = 0
         this.y = 0
-        this.kx = randomInRange(0.5, 0.5)
-        this.ky = randomInRange(0.5, 0.5)
-        this.walkRadius = point(randomInRange(50, 50), randomInRange(50, 50))
-        this.r = innerWidth / randomInRange(100, 150);
+        this.kx = random(0.5, 0.5)
+        this.ky = random(0.5, 0.5)
+        this.walkRadius = point(random(maxRadius, maxRadius), random(maxRadius, maxRadius))
+        this.r = innerWidth / random(100, 150);
     }
 
-    override update(_time: number, _delta: number) {
-        this.pointGraphics.clear()
-        this.lineGraphics.clear()
+    update(_time: number, _delta: number) {
+        this.graphics.clear()
         this.tick(_time / 1000)
     }
 
-    paintPoint(point: Point) {
-        this.points2.forEach((pt2) => {
-            if (!point.len)
+    paintCircle(circle: Circle) {
+        this.points.forEach((pt2) => {
+            if (!circle.len)
                 return
             this.drawLine(
-                lerp(this.x + pt2.x * this.r, point.x, point.len * point.len),
-                lerp(this.y + pt2.y * this.r, point.y, point.len * point.len),
+                lerp(this.x + pt2.x * this.r, circle.x, circle.len * circle.len),
+                lerp(this.y + pt2.y * this.r, circle.y, circle.len * circle.len),
                 this.x + pt2.x * this.r,
                 this.y + pt2.y * this.r
             );
         });
-        this.drawCircle(point.x, point.y, point.r!);
+        this.drawCircle(circle);
     }
 
     tick(time: number) {
@@ -86,46 +97,47 @@ export class Spider extends Phaser.GameObjects.Graphics {
         this.y += Math.min(innerWidth / 100, (fy - this.y) / 10)
 
         let i = 0
-        this.points.forEach((point) => {
-            const distanceFromPointToSpider = Math.hypot(point.x - this.x, point.y - this.y);
-            let newPointRadius = Math.min(2, innerWidth / distanceFromPointToSpider / 5);
-            point.t = 0;
-            const isIncreasing = distanceFromPointToSpider < innerWidth / 10
+        this.circles.forEach((circle) => {
+            const distanceFromCircleToSpider = Math.hypot(circle.x - this.x, circle.y - this.y);
+            let newCircleRadius = Math.min(2, innerWidth / distanceFromCircleToSpider / 5);
+            const isIncreasing = distanceFromCircleToSpider < innerWidth / 10
                 && (i++) < 8;
             const direction = isIncreasing ? 0.1 : -0.1;
             if (isIncreasing) {
-                newPointRadius *= 1.5;
+                newCircleRadius *= 1.5;
             }
-            point.r = newPointRadius;
-            point.len = Math.max(0, Math.min(point.len! + direction, 1));
-            this.paintPoint(point)
+            circle.radius = newCircleRadius;
+            circle.len = Math.max(0, Math.min(circle.len + direction, 1));
+            this.paintCircle(circle)
         });
     }
 
-
-    drawCircle(x: number, y: number, r: number) {
-        this.pointGraphics.fillCircle(x, y, r)
-
+    drawCircle(circle : Circle) {
+        this.graphics.fillCircle(circle.x, circle.y, circle.radius)
     }
 
     drawLine(x0: number, y0: number, x1: number, y1: number) {
-        this.lineGraphics.beginPath()
-        this.lineGraphics.moveTo(x0, y0)
+        this.graphics.beginPath()
+        this.graphics.moveTo(x0, y0)
         const n = 100;
         many(n, (i) => {
             i = (i + 1) / n;
             const x = lerp(x0, x1, i);
             const y = lerp(y0, y1, i);
             const k = noise(x / 5 + x0, y / 5 + y0) * 2;
-            this.lineGraphics.lineTo(x + k, y + k);
+            this.graphics.lineTo(x + k, y + k);
         });
-        this.lineGraphics.closePath()
-        this.lineGraphics.stroke()
+        this.graphics.closePath()
+        this.graphics.stroke()
     }
 }
 
-function randomInRange(max = 1, offset = 0) {
-    return Math.random() * max + offset;
+function random(scale = 1, offset = 0) {
+    return Math.random() * scale + offset;
+}
+
+function randomInRange(a: number, b: number) {
+    return Math.random() * (b - a) + a;
 }
 
 function many(n: number, f: (i: number) => any) {

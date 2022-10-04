2ae2929808c09e72a7f3075cb50bcb5e44681f80
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-10-04 20:39:55 +0200

    Remove the player from the scene
    Instead centers the camera on the spider

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,11 +1,9 @@
 import Phaser from 'phaser';
 import assets from "../assets";
-import {Player} from "../gameobjects/Player";
 import {Spider} from "../gameobjects/Spider";
 import {semanticColors} from "./colors";
 
 export default class MainScene extends Phaser.Scene {
-    private player: Player;
     private spider: Spider;
     private textPath: Phaser.GameObjects.Text[];
 
@@ -14,20 +12,17 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet(assets.dude.key, assets.dude.filepath,
-            {frameWidth: 32, frameHeight: 48}
-        );
         this.load.audio(assets.sounds.notification.key, assets.sounds.notification.filepath);
     }
 
     create() {
         this.cameras.main.setZoom(0.5);
-        this.cameras.main.centerOn(0, 0);
 
-        this.player = new Player(this)
         this.spider = new Spider(this)
+        this.cameras.main.centerOn(this.spider.x, this.spider.y);
 
-        this.cameras.main.startFollow(this.player.sprite, true, 0.09, 0.09);
+        const lerpingFactor = 0.2
+        this.cameras.main.startFollow(this.spider, true, lerpingFactor, lerpingFactor);
 
         const points = [
             20, 550,
@@ -50,7 +45,6 @@ export default class MainScene extends Phaser.Scene {
     }
 
     update(_time: number, _delta: number) {
-        this.player.update(_time, _delta)
         this.spider.update(_time, _delta)
         this.textPath.forEach(ch => ch.setRotation(ch.rotation + (Math.random() > 0.5 ? .01 : -.01)))
     }

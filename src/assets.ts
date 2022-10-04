655cedc3acaf1ab6916b82054930fe39d54cb598
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-10-04 22:20:39 +0200

    Remove unused assets

--- src/assets.ts
+++ src/assets.ts
@@ -1,39 +1,4 @@
 export default {
-    terrain : {
-        key: "terrain",
-        filepath: "assets/terrain.svg"
-    },
-    platforms: {
-        platform1 : {
-            key: 'platform1',
-            filepath: 'assets/platforms/platform1.png'
-        },
-        platform2 : {
-            key: 'platform2',
-            filepath: 'assets/platforms/platform2.png'
-        },
-        platform3 : {
-            key: 'platform3',
-            filepath: 'assets/platforms/platform3.png'
-        },
-        platform4 : {
-            key: 'platform4',
-            filepath: 'assets/platforms/platform4.png'
-        },
-
-    },
-    star: {
-        key: 'star',
-        filepath: 'assets/star.png'
-    },
-    bomb: {
-        key: 'bomb',
-        filepath: 'assets/bomb.png'
-    },
-    dude: {
-        key: 'dude',
-        filepath:      'assets/dude.png'
-    },
     sounds : {
         notification : {
             key: "notification",

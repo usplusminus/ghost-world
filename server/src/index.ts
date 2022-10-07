5a2a2e8ac03ade137cfc15779d6f82bc7189824b
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-10-07 15:08:38 +0200

    Remove all attempts at sockets for syncing screens

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import {setupSocketServer} from "./server";
-
-function main() {
-    console.log("Starting socket server for the ghost-world")
-    const server = setupSocketServer()
-    server.listen(5555);
-}
-
-main()

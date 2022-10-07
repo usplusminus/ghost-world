3ad6af827b0308d87e3216b95deae1064b86cd84
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-10-07 14:09:58 +0200

    Set up basic socket connection on the client side

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,14 @@
+import { io } from "socket.io-client";
 import './style.css'
 import {setupGame} from "./gameSetup";
+
+
+const socketClient = io("http://localhost:5555")
+socketClient.connect()
+
+socketClient.on("connect", () => {
+    console.log("Client ID", socketClient.id);
+});
 
 const appRoot = document.querySelector<HTMLDivElement>('#app')!
 

97b74fd29d0c7e690fc803617daaf0f37e9abbb3
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-10-07 14:08:36 +0200

    Relax CORS settings to allow all to connect

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,11 @@ import {Server} from "socket.io";
 
 
 export function setupSocketServer(){
-    const server = new Server({ /* options */ });
+    const server = new Server({
+        cors: {
+            origin: "*",
+        }
+    });
 
     server.on("connection", (socket) => {
         console.log("ID:", socket.id, "connected")

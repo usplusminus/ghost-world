c31b5c05cc07d6b6d832d81fc2e6a97fbf273d3f
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-10-08 19:51:01 +0200

    MVP of debouncing effect on interactions based on interval

--- src/gameobjects/Interactable.ts
+++ src/gameobjects/Interactable.ts
@@ -14,6 +14,7 @@ export default class Interactable extends Phaser.GameObjects.Graphics {
     private readonly graphics: Phaser.GameObjects.Graphics;
     private readonly position: Phaser.Math.Vector2;
     private readonly radius: number;
+    private lastInteraction: number;
 
     constructor(
         scene: Phaser.Scene,
@@ -28,14 +29,18 @@ export default class Interactable extends Phaser.GameObjects.Graphics {
             lineStyle: {width: 0.5, color: HexColor.white, alpha: 1.0}
         })
         this.graphics.fillCircle(this.position.x, this.position.y, this.radius)
+        const requiredIntervalMs = 1000
+        this.lastInteraction = Date.now() - requiredIntervalMs // manually set it back
 
         eventEmitter.on(GameEvent.SPIDER_POSITION_UPDATED, (x: number, y: number) => {
-            if (this.position.distance({ x, y }) < this.radius)
-                this.broadcastInteraction()
+            if (!(this.position.distance({ x, y }) < this.radius)) return
+            if (Date.now() - this.lastInteraction < requiredIntervalMs) return
+            this.broadcastInteraction()
         })
     }
 
     broadcastInteraction() {
+        this.lastInteraction = Date.now()
         sendTextToScreen1(sampleList(texts))
     }
 }
